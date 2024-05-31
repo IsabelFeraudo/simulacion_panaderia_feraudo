a83aed2ff9d66d91d50ed1d2b834e03e2b6020c9
IsabelFeraudo <natyferaudo23@gmail.com>
2024-05-31 19:40:59 -0300

    initial classes

--- simulation/classes.py
+++ simulation/classes.py
@@ -38,9 +38,10 @@ class Evento(ABC):
         self.valor_reloj = 0
    
 class LlegadaCliente(Evento):
-    def __init__(self, estado="Libre", *args, **kwargs):
+    def __init__(self, estado="Libre", id_cliente=0,*args, **kwargs):
         super().__init__(*args, **kwargs)
         self.estado = estado
+        self.id_cliente = id_cliente
 
     def to_dict(self):
         d = {}
@@ -60,10 +61,11 @@ class LlegadaCliente(Evento):
         self.id_grupo = 0
 
 class Cliente:
-    def __init__(self, id, estado, hora_llegada=0):
+    def __init__(self, id, estado, hora_llegada=0,abandona=False):
         self.id = id
         self.estado = estado
         self.hora_llegada = hora_llegada
+        self.abandona= abandona
 
 
     def to_dict(self):
@@ -71,3 +73,104 @@ class Cliente:
         for llave, valor in self.__dict__.items():
             d[f"G {self.id} - {llave}"] = valor
         return d
+    
+    def calc_cant_productos(self):
+        self.rnd_cant_personas = get_rand()
+        for i in PROB_PRODUCTOS:
+            if i[1] <= self.rnd_cant_productos <= i[2]:
+                self.cant_productos = i[0]
+                
+    #calcular abandono
+
+#COMO HACERLO PARA DOS CAJAS?
+class FinAtencion(Evento):
+    def __init__(self, estado="Libre", id_cliente=0,*args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.estado = estado
+        self.id_cliente = id_cliente
+
+    def to_dict(self):
+        d = {}
+        for llave, valor in self.__dict__.items():
+            if llave == "id" or llave == "valor_formula":
+                continue
+            d[f"Cliente Est {self.id} - {llave}"] = valor
+        return d
+
+    def calc_valor_reloj(self, reloj):
+        self.rand = get_rand()
+        self.valor = 0.5+(self.rand)*(1.5-0.5)
+        self.valor_reloj = reloj + self.valor
+
+    def reset(self):
+        super(FinAtencion, self).reset()
+        self.id_cliente = 0
+        
+class FinEspera(Evento):
+    def __init__(self, id_cliente=0,*args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.id_cliente = id_cliente
+
+    def to_dict(self):
+        d = {}
+        for llave, valor in self.__dict__.items():
+            if llave == "id" or llave == "valor_formula":
+                continue
+            d[f"Cliente Est {self.id} - {llave}"] = valor
+        return d
+
+    def calc_valor_reloj(self, reloj):
+        self.rand = get_rand()
+        self.valor = 5
+        self.valor_reloj = reloj + self.valor
+
+    def reset(self):
+        super(FinEspera, self).reset()
+        self.id_cliente = 0
+        
+class FinCoccion(Evento):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+      
+
+    def to_dict(self):
+        d = {}
+        for llave, valor in self.__dict__.items():
+            if llave == "id" or llave == "valor_formula":
+                continue
+            d[f"Cliente Est {self.id} - {llave}"] = valor
+        return d
+
+    def calc_valor_reloj(self, reloj):
+        self.rand = get_rand()
+        self.valor = 0 #programar ec dif
+        self.valor_reloj = reloj + self.valor
+
+    def reset(self):
+        super(FinCoccion ,self).reset()
+        self.id_cliente = 0
+        
+class ProximoEncendido(Evento):
+    def __init__(self, estado="Apagado",*args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.estado = estado
+    
+    def to_dict(self):
+        d = {}
+        for llave, valor in self.__dict__.items():
+            if llave == "id" or llave == "valor_formula":
+                continue
+            d[f"Horno Est {self.id} - {llave}"] = valor
+        return d
+
+    def calc_valor_reloj(self, reloj):
+        self.rand = get_rand()
+        #calcular valor de proximo encendido. vada 45 min o cuando no haya stock
+        self.valor = 0
+        self.valor_reloj = reloj + self.valor
+
+    def reset(self):
+        super(FinAtencion, self).reset()
+        self.id_cliente = 0
+        
+#clase panadero 1 y 2?
